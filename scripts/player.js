0f8c93ebb2c6d6e0fdb0222d0af36e8f42abb279
Parzava <mccartneybl@s.dcsdk12.org>
2024-10-07 15:48:44 -0600

    Made your code better ;)

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,3 @@
-//@ts-check
 import { canvas, ctx } from "./common/canvas.js";
 
 export class Player {
@@ -7,115 +6,31 @@ export class Player {
 		this.y = y;
 		this.width = 75;
 		this.height = 25;
-
 		this.speed = 5;
 
-		this.keyBindings = {
-			up: "ArrowUp",
-			down: "ArrowDown",
-			right: "ArrowRight",
-			left: "ArrowLeft",
-		};
-
-		this.moving = {
-			up: false,
-			down: false,
-			right: false,
-			left: false,
-		};
+		this.moving = { up: false, down: false, right: false, left: false };
+		this.keyBindings = { ArrowUp: "up", ArrowDown: "down", ArrowRight: "right", ArrowLeft: "left" };
 
 		this.wireUpKeyboard();
 	}
 
 	wireUpKeyboard() {
-		window.addEventListener("keydown", (e) => {
-			//console.log(e);
+		window.addEventListener("keydown", (e) => this.toggleMovement(e, true));
+		window.addEventListener("keyup", (e) => this.toggleMovement(e, false));
+	}
 
-			switch (e.code) {
-				case this.keyBindings.up:
-					this.moving.up = true;
-					break;
-				case this.keyBindings.down:
-					this.moving.down = true;
-					break;
-				case this.keyBindings.right:
-					this.moving.right = true;
-					break;
-				case this.keyBindings.left:
-					this.moving.left = true;
-					break;
-			}
-		});
-
-		window.addEventListener("keyup", (e) => {
-			//console.log(e);
-			switch (e.code) {
-				case this.keyBindings.up:
-					this.moving.up = false;
-					break;
-				case this.keyBindings.down:
-					this.moving.down = false;
-					break;
-				case this.keyBindings.right:
-					this.moving.right = false;
-					break;
-				case this.keyBindings.left:
-					this.moving.left = false;
-					break;
-			}
-		});
+	toggleMovement(e, isMoving) {
+		if (this.keyBindings[e.code]) {
+			this.moving[this.keyBindings[e.code]] = isMoving;
+		}
 	}
 
 	update() {
-		let dirX = 0;
-		let dirY = 0;
+		let dirX = (this.moving.right ? 1 : 0) - (this.moving.left ? 1 : 0);
+		let dirY = (this.moving.down ? 1 : 0) - (this.moving.up ? 1 : 0);
 
-		if (this.moving.up) {
-			dirY = -1;
-		}
-
-		if (this.moving.down) {
-			dirY = 1;
-		}
-
-		if (this.moving.up && this.moving.down) {
-			dirY = 0;
-		}
-
-		if (this.moving.left) {
-			dirX = -1;
-		}
-
-		if (this.moving.right) {
-			dirX = 1;
-		}
-
-		if (this.moving.left && this.moving.right) {
-			dirX = 0;
-		}
-
-		this.x += this.speed * dirX;
-		this.y += this.speed * dirY;
-
-		// prevent going off left
-		if (this.x < 0) {
-			this.x = 0;
-		}
-
-		// prevent going off top
-		if (this.y < 0) {
-			this.y = 0;
-		}
-
-		// prevent from going off right
-		if (this.x + this.width > canvas.width) {
-			this.x = canvas.width - this.width;
-		}
-
-		// prevent from going off bottom
-		if (this.y + this.height > canvas.height) {
-			this.y = canvas.height - this.height;
-		}
+		this.x = Math.max(0, Math.min(this.x + this.speed * dirX, canvas.width - this.width));
+		this.y = Math.max(0, Math.min(this.y + this.speed * dirY, canvas.height - this.height));
 	}
 
 	draw() {
